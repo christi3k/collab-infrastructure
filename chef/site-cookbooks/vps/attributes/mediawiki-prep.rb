995145337f27eb7f2c7a344d628257cd74a1008a
Christie Koehler <ck@christi3k.net>
2017-07-19 12:29:20 -0700

    Add recipe preparing for mediawiki install.

---
+++ chef/site-cookbooks/vps/attributes/mediawiki-prep.rb
@@ -0,0 +1,6 @@
+# Cookbook Name:: vps
+# Attributes:: mediawiki-prep
+
+default['mediawiki']['git']['repository'] = 'git@github.com:numfocus/wiki.numfocus.org.git'
+#default['mediawiki']['git']['reference'] = ''
+
